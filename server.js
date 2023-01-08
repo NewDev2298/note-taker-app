cb58006fbf13b8f3f1608426729244f47e84acfa
Terrance Smith <terrancesmith@Laquishas-Air.lan>
2023-01-08 18:10:32 -0500

    added middleware and routes

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Note Taker');
+  // res.send('Note Taker');
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-app.listen(PORT);
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+
+});
+
+app.post('/api/notes', (req, res) => {
+
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on PORT ${PORT}`);
+}); 

065e34c8f686957502bb92c0aa33c8e5182fff3b
Terrance Smith <terrancesmith@Laquishas-Air.lan>
2023-01-08 20:23:31 -0500

    trying different additions for routes

--- server.js
+++ server.js
@@ -9,22 +9,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Displays Note Taker initial page
 app.get('/', (req, res) => {
-  // res.send('Note Taker');
   res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
+// Displays Note Taker content page
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
+// GET Route returns saved notes
 app.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 
 });
 
+// POST Route saves new notes
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} new note saved`);
+
+  const newNote = req.body;
+
+  if
 });
 
 app.delete('/api/notes/:id', (req, res) => {
